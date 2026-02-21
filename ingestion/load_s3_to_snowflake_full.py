ee48bfe5b1a8c83c3d103ea326884645c5517031
VigneshSundar0417 <s.vigneshri@hotmail.com>
2026-02-21 15:42:28 -0800

    Change Snowflake user, password, and account
    Updated Snowflake configuration credentials.

--- ingestion/load_s3_to_snowflake_full.py
+++ ingestion/load_s3_to_snowflake_full.py
@@ -11,9 +11,9 @@ file_key = "bike_sales.csv"   # exact file name in S3
 # ----------------------------
 # Snowflake Configuration
 # ----------------------------
-sf_user = "vignesh1784"
-sf_password = "Vignesh@1984!!"        # <-- your Snowflake password
-sf_account = "SGIVFZX-XFB94552"               # <-- Snowflake account identifier only
+sf_user = "vi"
+sf_password = "Vig"        # <-- your Snowflake password
+sf_account = "SGIVF"               # <-- Snowflake account identifier only
 sf_warehouse = "MY_WH"
 sf_role = "ACCOUNTADMIN"
 sf_database = "MY_DATA_PIPELINE"
